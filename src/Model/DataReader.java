c4a1e859d852a022f6bf71b44ab5cb66c817c6cb
Philipp-p <Philipp-p@users.noreply.github.com>
2015-01-15 18:31:26 +0100

    added a Manager and improved the DataReader

--- src/Model/DataReader.java
+++ src/Model/DataReader.java
@@ -1,46 +1,46 @@
 package Model;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-
-import quadTree.Points;
 import quadTree.QuadTree;
 
 public class DataReader {
-	QuadTree<Points> min90 = new QuadTree<Points>(11, 47, 12, 48);
-	QuadTree<Points> min180 = new QuadTree<Points>(11, 47, 12, 48);
-	QuadTree<Points> parkstrassen = new QuadTree<Points>(11, 47, 12, 48);
+	protected QuadTree<Points> min90 = new QuadTree<Points>(11, 47, 12, 48);
+	protected QuadTree<Points> min180 = new QuadTree<Points>(11, 47, 12, 48);
+	protected QuadTree<Points> parkstrassen = new QuadTree<Points>(11, 47, 12,
+			48);
 
-	public static void demoRead(BufferedReader buff) throws IOException {
-		for (String content = buff.readLine(); content != null; content = buff
-				.readLine()) {
-			String[] tmp = content.split(";");
-			System.out.println(tmp[3] + " " + tmp[6] + " " + tmp[7]);
-		}
-	}
-
-	public void fillTrees(BufferedReader buff) throws IOException {
+	/**
+	 * fills the tree quad trees of the DataReader, one for each category of
+	 * master meters, with the data from www.innsbruck.gv.at
+	 * 
+	 * @throws IOException
+	 */
+	public void fillTrees() throws IOException {
+		BufferedReader buff = loadFileFromWeb();
 		Double x;
 		Double y;
 		int i = 0;
-		for (String content = buff.readLine() ; content != null; content = buff.readLine(), i++) {
+		for (String content = buff.readLine(); content != null; content = buff
+				.readLine(), i++) {
 			String[] tmp = content.split(";");
-			if (i !=0) {
-				x = Double.parseDouble(tmp[6].replace(",","."));
-				y = Double.parseDouble(tmp[7].replace(",","."));
+			if (i != 0) {
+				x = Double.parseDouble(tmp[6].replace(",", ".")); // needed because parseDouble wants "."
+				y = Double.parseDouble(tmp[7].replace(",", "."));
 				if (tmp[3].equals("90 min."))
-					min90.put(x, y, new Points(x,y));
+					min90.put(x, y, new Points(x, y));
 				else if (tmp[3].equals("180 min."))
-					min180.put(x, y, new Points(x,y));
+					min180.put(x, y, new Points(x, y));
 				else if (tmp[3].equals("Parkstraße werktags"))
-					parkstrassen.put(x, y, new Points(x,y));
+					parkstrassen.put(x, y, new Points(x, y));
 			}
 		}
 	}
 
-	public static BufferedReader loadFileFromWeb() {
+	private static BufferedReader loadFileFromWeb() {
 		try {
 			URL url12 = new URL(
 					"https://www.innsbruck.gv.at/data.cfm?vpath=diverse/ogd/gis/parkscheinautomatencsv");
@@ -55,6 +55,14 @@ public class DataReader {
 		return null;
 	}
 
+	public static void demoRead(BufferedReader buff) throws IOException {
+		for (String content = buff.readLine(); content != null; content = buff
+				.readLine()) {
+			String[] tmp = content.split(";");
+			System.out.println(tmp[3] + " " + tmp[6] + " " + tmp[7]);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		BufferedReader buff = loadFileFromWeb();
@@ -66,7 +74,7 @@ public class DataReader {
 		buff = loadFileFromWeb();
 		DataReader test = new DataReader();
 		try {
-			test.fillTrees(buff);
+			test.fillTrees();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
